779f6ee8e3de80c4d37c13231c05046172dc8686
Christ-ok <christopherkiaku48@gmail.com>
2025-12-15 16:48:15 +0100

    Tentative de merge main et deplacementBackground

--- src/background.c
+++ src/background.c
@@ -4,21 +4,27 @@
 
 SDL_Texture *textureBackground = NULL;
 
-void initBackground(SDL_Renderer *renderer)
-{
+void initBackground(SDL_Renderer *renderer){
     textureBackground = IMG_LoadTexture(renderer, "assets/background/PNG/City2/Bright/City2.png");
     if (!textureBackground)
         SDL_Log("Erreur chargement background: %s", IMG_GetError());
 }
 
-void drawBackground(SDL_Renderer *renderer)
-{
-    if (textureBackground)
-        SDL_RenderCopy(renderer, textureBackground, NULL, NULL);
+void drawBackground(SDL_Renderer *renderer, int x){
+    if (!textureBackground){
+        return;
+    }
+
+    SDL_Rect dst;
+    dst.x = x;
+    dst.y = 0;
+    dst.w = 800;
+    dst.h = 600;
+
+    SDL_RenderCopy(renderer, textureBackground, NULL, &dst);
 }
 
-void cleanupBackground()
-{
+void cleanupBackground(){
     if (textureBackground)
         SDL_DestroyTexture(textureBackground);
 }
--- src/background.h
+++ src/background.h
@@ -3,8 +3,8 @@
 
 #include <SDL2/SDL.h>
 
-void initBackground(SDL_Renderer *renderer);
-void drawBackground(SDL_Renderer *renderer);
+void initBackground(SDL_Renderer *renderer); 
+void drawBackground(SDL_Renderer *renderer, int x);
 void cleanupBackground();
 
 #endif
